2b49c4cd7bec9aea3f38d1b20ba281bcaadd5974
Inze Bielderman <Bielderman@InzeNL.com>
2018-12-04 20:00:08 +0100

    Day 4

---
+++ d4p2.js
@@ -0,0 +1,25 @@
+const md5 = require('md5');
+
+let input = 'yzbqklnj';
+
+let hashNumber = null;
+let i = 1;
+while (hashNumber === null) {
+    if (createsValidHash(input, i)) {
+        hashNumber = i;
+    }
+    i++;
+}
+
+console.log(hashNumber);
+
+function createsValidHash(input, number) {
+    let hash = md5(input + number);
+    let validHash = true;
+    for (let i = 0; i < 6; i++) {
+        if (hash.charAt(i) !== '0') {
+            validHash = false;
+        }
+    }
+    return validHash;
+}

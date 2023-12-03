b338131e82abc1b606c799b32c658da5e6a6cd19
Inze Bielderman <Bielderman@InzeNL.com>
2023-12-03 10:03:12 +0100

    Adds total run time

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,16 @@ mod day2;
 mod day3;
 
 fn main() {
+    use std::time::Instant;
+    let now = Instant::now();
+
     day1::part1();
     day1::part2();
     day2::part1();
     day2::part2();
     day3::part1();
     day3::part2();
+
+    let elapsed = now.elapsed();
+    println!("Total run time: {:?}", elapsed);
 }

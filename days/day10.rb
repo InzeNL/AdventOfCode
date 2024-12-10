41f62749c61eb8ab201777bbd6477cfb040e68a5
Inze Bielderman <Bielderman@InzeNL.com>
2024-12-10 08:06:16 +0100

    Removes duplicate code

--- days/day10.rb
+++ days/day10.rb
@@ -26,7 +26,7 @@ class Day10
     return sum
   end
 
-  def get_trail_heads(lines, coordinate)
+  def get_trail_heads(lines, coordinate, get_ratings = false)
     x = coordinate[0]
     y = coordinate[1]
 
@@ -72,7 +72,11 @@ class Day10
     end
 
     if (current_number == 0)
-      return coordinates.uniq().count()
+      if (get_ratings)
+        return coordinates.count()
+      else
+        return coordinates.uniq().count()
+      end
     else
       return coordinates
     end
@@ -103,61 +107,9 @@ class Day10
     end
 
     coordinates.each do |coordinate|
-      sum += get_trail_head_ratings(lines, coordinate)
+      sum += get_trail_heads(lines, coordinate, true)
     end
 
     return sum
-  end
-
-  def get_trail_head_ratings(lines, coordinate)
-    x = coordinate[0]
-    y = coordinate[1]
-
-    current_number = Integer(lines[y][x])
-
-    if (current_number == 9)
-      return [coordinate]
-    end
-
-    up_x = x
-    up_y = y - 1
-
-    right_x = x + 1
-    right_y = y
-
-    down_x = x
-    down_y = y + 1
-
-    left_x = x - 1
-    left_y = y
-
-    up = get_integer_value_for_coordinate(lines, up_x, up_y)
-    right = get_integer_value_for_coordinate(lines, right_x, right_y)
-    down = get_integer_value_for_coordinate(lines, down_x, down_y)
-    left = get_integer_value_for_coordinate(lines, left_x, left_y)
-
-    coordinates = []
-
-    if (up == current_number + 1)
-      coordinates += get_trail_head_ratings(lines, [up_x, up_y])
-    end
-
-    if (right == current_number + 1)
-      coordinates += get_trail_head_ratings(lines, [right_x, right_y])
-    end
-
-    if (down == current_number + 1)
-      coordinates += get_trail_head_ratings(lines, [down_x, down_y])
-    end
-
-    if (left == current_number + 1)
-      coordinates += get_trail_head_ratings(lines, [left_x, left_y])
-    end
-
-    if (current_number == 0)
-      return coordinates.count()
-    else
-      return coordinates
-    end
   end
 end

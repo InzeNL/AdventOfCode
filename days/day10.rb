16c27d33ff123ffb0fdf9f7b225a20ec5f1b9800
Inze Bielderman <Bielderman@InzeNL.com>
2024-12-10 07:49:41 +0100

    Implements day 10 part 2

--- days/day10.rb
+++ days/day10.rb
@@ -85,4 +85,79 @@ class Day10
 
     return Integer(lines[y][x])
   end
+
+  def part2()
+    lines = @@InputParserHelper.get_lines()
+
+    sum = 0
+
+    joined_lines = lines.join()
+
+    i = -1
+    coordinates = []
+    while (i = joined_lines.index('0', i + 1))
+      x = i % lines[0].length()
+      y = (i / lines[0].length()).floor()
+
+      coordinates << [x, y]
+    end
+
+    coordinates.each do |coordinate|
+      sum += get_trail_head_ratings(lines, coordinate)
+    end
+
+    return sum
+  end
+
+  def get_trail_head_ratings(lines, coordinate)
+    x = coordinate[0]
+    y = coordinate[1]
+
+    current_number = Integer(lines[y][x])
+
+    if (current_number == 9)
+      return [coordinate]
+    end
+
+    up_x = x
+    up_y = y - 1
+
+    right_x = x + 1
+    right_y = y
+
+    down_x = x
+    down_y = y + 1
+
+    left_x = x - 1
+    left_y = y
+
+    up = get_integer_value_for_coordinate(lines, up_x, up_y)
+    right = get_integer_value_for_coordinate(lines, right_x, right_y)
+    down = get_integer_value_for_coordinate(lines, down_x, down_y)
+    left = get_integer_value_for_coordinate(lines, left_x, left_y)
+
+    coordinates = []
+
+    if (up == current_number + 1)
+      coordinates += get_trail_head_ratings(lines, [up_x, up_y])
+    end
+
+    if (right == current_number + 1)
+      coordinates += get_trail_head_ratings(lines, [right_x, right_y])
+    end
+
+    if (down == current_number + 1)
+      coordinates += get_trail_head_ratings(lines, [down_x, down_y])
+    end
+
+    if (left == current_number + 1)
+      coordinates += get_trail_head_ratings(lines, [left_x, left_y])
+    end
+
+    if (current_number == 0)
+      return coordinates.count()
+    else
+      return coordinates
+    end
+  end
 end
